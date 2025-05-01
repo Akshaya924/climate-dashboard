a59da4e64033da96ea82d6f495ad71550547bc72
Akshaya924 <akshaya.20233116@iit.ac.lk>
2025-05-01 21:10:11 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="🌿 Sri Lanka Climate Dashboard", layout="wide")
 # --- Load Data ---
 @st.cache_data
 def load_data():
-    df = pd.read_csv("climate-change_lka.csv")
+    df = pd.read_csv("climate-change_lka_cleaned.csv")
     df = df.dropna()
     return df
 
